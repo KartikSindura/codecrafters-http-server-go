47aa253316e1af76d60b98d3961c82579657da46
KartikSindura <kartik.sindura@gmail.com>
2024-04-22 21:00:42 +0530

    stage 3

--- app/server.go
+++ app/server.go
@@ -2,16 +2,31 @@ package main
 
 import (
 	"fmt"
+	"log"
 	// Uncomment this block to pass the first stage
 	"net"
 	"os"
+	"strings"
 )
 
-func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
+func do(conn net.Conn) {
+	buf := make([]byte, 1024)
+	_, err := conn.Read(buf)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	res := string(buf)
+	lines := strings.Split(res, "\r\n")
+	path := strings.Split(lines[0], " ")
+	if path[1] == "/" {
+		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	} else {
+		conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+	}
 
-	// Uncomment this block to pass the first stage
+}
+
+func main() {
 
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
@@ -19,14 +34,13 @@ func main() {
 		os.Exit(1)
 	}
 
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
-	_, err = conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
-	if err != nil {
-		fmt.Println("Error write connection: ", err.Error())
-		os.Exit(1)
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+
+		go do(conn)
 	}
 }
